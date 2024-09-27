f54458f7eb862ad9252fb86ca971dab7f3aa9782
saidurraahmaan <saidurraahmaan@gmail.com>
2024-09-28 00:57:15 +0600

    Add account mapper controller repo

---
+++ accounts/src/main/java/com/s4r/accounts/dto/AccountsDto.java
@@ -0,0 +1,68 @@
+package com.s4r.accounts.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+
+public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
+//    @Schema(
+//            description = "Account Number of Eazy Bank account", example = "3454433243"
+//    )
+    private Long accountNumber;
+
+    @NotEmpty(message = "AccountType can not be a null or empty")
+//    @Schema(
+//            description = "Account type of Eazy Bank account", example = "Savings"
+//    )
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+//    @Schema(
+//            description = "Eazy Bank branch address", example = "123 NewYork"
+//    )
+    private String branchAddress;
+
+    public AccountsDto() {
+    }
+
+    public AccountsDto(Long accountNumber, String accountType, String branchAddress) {
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.branchAddress = branchAddress;
+    }
+
+    public Long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(Long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBranchAddress() {
+        return branchAddress;
+    }
+
+    public void setBranchAddress(String branchAddress) {
+        this.branchAddress = branchAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountsDto{" +
+                "accountNumber=" + accountNumber +
+                ", accountType='" + accountType + '\'' +
+                ", branchAddress='" + branchAddress + '\'' +
+                '}';
+    }
+}
---
+++ accounts/src/main/java/com/s4r/accounts/mapper/AccountMapper.java
@@ -0,0 +1,20 @@
+package com.s4r.accounts.mapper;
+
+import com.s4r.accounts.dto.AccountsDto;
+import com.s4r.accounts.entity.Accounts;
+
+public class AccountMapper {
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
