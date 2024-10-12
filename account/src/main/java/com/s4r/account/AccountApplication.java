b39c6778bb8e72a235ff559cc81a07612a08a2e5
saidurraahmaan <saidurraahmaan@gmail.com>
2024-10-12 17:57:07 +0600

    Add Dockerfile and fix card auditing

--- account/src/main/java/com/s4r/account/AccountApplication.java
+++ account/src/main/java/com/s4r/account/AccountApplication.java
@@ -1,13 +1,37 @@
 package com.s4r.account;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Bank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Saidur Rahman",
+                        email = "saidur.r4n@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Bank Accounts microservice REST API Documentation"
+        )
+)
 public class AccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
 
 }
--- card/src/main/java/com/s4r/card/entity/Card.java
+++ card/src/main/java/com/s4r/card/entity/Card.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 
 
 @Entity
-public class Card {
+public class Card extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cardId;
